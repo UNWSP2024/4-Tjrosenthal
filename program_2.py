8ee1a887454cc696e4dd9c7053a13dd7043825c3
Tjrosenthal <tjrosenthal@students.unwsp.edu>
2025-02-12 13:55:55 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -4,11 +4,23 @@
 # At the end of the program it prints out the total number of tickets desired by the user.  
 # Use either a "for loop" or "while loop" to accomplish this.
 
-def main():
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
 
+#Tanner Rosenthal
+#2/12/2025
+
+def main():
+
+total_tickets = 0
+
+while True:
+    movie_name = input("What movie would you like to see? (type 'done' to finish)")
+    if movie_name == "done":
+        break
+
+    ticket_number = int(input("How many tickets would you like?"))
+    total_tickets += ticket_number
+
+print("You bought", total_tickets, "tickets")
 
 if __name__ == '__main__':
     main()
