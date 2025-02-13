0623cd291f8df3936bfaa65c3893e5e66c119c4f
Tjrosenthal <tjrosenthal@students.unwsp.edu>
2025-02-13 10:03:44 -0600

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -5,15 +5,32 @@
 # When the loop finishes, the program should display the amount that the 
 # user is over or under budget.
 
-def main():
-    budget = 0.0
-    difference = 0.0
-    spent = 1.0         #initialize for while loop
-    total = 0.0
 
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+#Tanner Rosenthal
+#2/13/2025
+def main():
+
+expenses = 0.0
+total_cost = 0.0
+budget = float(input("What is your budget?"))
+
+while True:
+    cost = input("How much did your next item cost? (type 'done' to finish)")
+    if cost == "done":
+       break
+    else:
+        total_cost += float(cost)
+
+over_under = budget - total_cost
+
+if over_under<0:
+    print(f"You were ${abs(over_under):.2f} over budget!")
+
+elif over_under == 0:
+    print(f"You spent your budget of ${total_cost:.2f}")
+
+else:
+    print(f"You still have ${over_under:.2f} left of your budget")
 
 
 if __name__ == '__main__':
